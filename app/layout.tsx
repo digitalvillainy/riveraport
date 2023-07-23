cd11103841daf1069123d899fb714358e7d0c97f
Roberto Rivera <rrivera@pop-os.localdomain>
2023-07-23 00:06:09 -0400

    added new Image tags from nextjsl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import NavMenu from './NavMenu/NavMenu'
-
+import Image from 'next/image'
+import GitHubIcon from '../public/GitHub.svg'
+import BitBucketIcon from '../public/BitBucket.svg'
 
 export const metadata: Metadata = {
   title: 'Roberto Rivera | Front-End Web Developer',
@@ -20,9 +22,12 @@ export default function RootLayout({
       <body className='p-4'>
         <NavMenu />
         <div className='flex flex-row w-full'>
-          <span className="flex flwx-column place-items-end">a</span>
-          <section>
-            <main className='border-2 border-[#AD00EA] rounded-tl-2xl rounded-br-2xl my-5 ml-14 mr-2 p-5'>
+          <div className="flex flex-column place-items-end mb-10">
+            <Image src={GitHubIcon} alt='GitHub Icon' width={40} height={40} />
+            <Image src={BitBucketIcon} alt='BitBucket Icon' width={43} height={43} />
+          </div>
+          <section className='w-full'>
+            <main className='border-2 border-[#AD00EA] rounded-tl-2xl rounded-br-2xl my-5 ml-5 mr-2 p-5'>
               <div className='mb-4'>
                 <h3 className='text-3xl'>Roberto Rivera</h3>
                 <span className='text-sm'>Front-End Web Developer</span>
